f78e5586719954ad0fb9ce24cfe58cfc6060c70f
a.suprun <oxymoron.dr@gmail.com>
2019-12-13 15:42:42 +0200

    adaptive

--- js/main.js
+++ js/main.js
@@ -12,6 +12,11 @@ $(function() {
         slidesToShow: 4,
         slidesToScroll: 4,
         asNavFor: '.header__slider',
+        responsive: [{
+            breakpoint: 961,
+            settings: "unslick"
+        }, ]
+
     });
 
     $('.surf-slider').slick({
@@ -20,6 +25,30 @@ $(function() {
         prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt=""></img>',
         nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt=""></img>',
         asNavFor: '.slider-map',
+        responsive: [{
+            breakpoint: 1210,
+            settings: {
+                slidesToShow: 3,
+            }
+        }, {
+            breakpoint: 900,
+            settings: {
+                slidesToShow: 2,
+
+            }
+        }, {
+            breakpoint: 720,
+            settings: {
+                slidesToShow: 1,
+                centerMode: true,
+            },
+        }, {
+            breakpoint: 426,
+            settings: {
+                slidesToShow: 1,
+                centerMode: false,
+            },
+        }, ]
     });
 
     $('.slider-map').slick({
@@ -27,7 +56,27 @@ $(function() {
         slidesToScroll: 1,
         arrows: false,
         asNavFor: '.surf-slider',
-        focusOnSelect: true
+        focusOnSelect: true,
+        responsive: [{
+                breakpoint: 1103,
+                settings: {
+                    slidesToShow: 3,
+                }
+            }, {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 2,
+                    centerMode: true,
+                },
+            }, {
+                breakpoint: 720,
+                settings: {
+                    slidesToShow: 1,
+                    centerMode: true,
+                },
+            },
+
+        ]
     });
 
     $('.holder__slider, .shop__slider').slick({
@@ -74,11 +123,17 @@ $(function() {
         let summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() - 1) * $('.summ').data('guests');
         $('.summ').html('$' + summ);
     });
+
     let summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() - 1) * $('.summ').data('guests');
     $('.summ').html('$' + summ);
 
+
     $('.surfboard-box__circle').on('click', function() {
-        $(this).toggleClass('active')
+        $(this).toggleClass('active');
     });
 
+
+    $('.menu-btn').on('click', function() {
+        $('.menu').toggleClass('active');
+    });
 });
